61ade84ac87bace963805a1527043b830dd9b38f
Nasif-Hasan <nasifhasan35@gmail.com>
2022-10-25 17:27:57 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCq_SsH7qArwu6ofNh1TXQ3Y9Ie9__xpgU",
+  authDomain: "game-tricks-e9d4d.firebaseapp.com",
+  projectId: "game-tricks-e9d4d",
+  storageBucket: "game-tricks-e9d4d.appspot.com",
+  messagingSenderId: "745577220420",
+  appId: "1:745577220420:web:2176380fb26fcef47c2f11"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
